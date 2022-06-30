ae232c9d09f03557c294b763bbf3cd02341b57b8
Kenneth Andrews <eaandkw@yahoo.com>
2022-06-30 10:53:01 -0500

    added more files

---
+++ for_loops.py
@@ -0,0 +1,11 @@
+
+# for letter in "Giraffe Academy":
+#    print(letter)
+
+girls = ["Jennifer", "Karen", "Becky"]
+guys = ["Ken", "Bob", "Mike"]
+for friend in girls and guys:
+    print(friend)
+
+# for index in range(10):
+#    print(index)
